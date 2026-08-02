36447e3d3cd9487f51fab908e87cbcd66b9aff5d
Ahmad3150 <ahmadhassandy11@gmail.com>
2026-08-02 09:04:12 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// Show welcome message when page loads
+alert("Welcome to Naija Political Watch");
+
+// Function to show messages
+function showMessage() {
+    alert("Feature coming soon!");
+}
+
+// You can add more functions here later
+// Example: showMessage() will run when you click buttons
